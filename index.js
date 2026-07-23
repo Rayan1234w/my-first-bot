96319d62df1c015341290ec1db80469de44c93dc
Rayan1234w <167709478+Rayan1234w@users.noreply.github.com>
2026-07-23 17:20:55 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -458,4 +458,17 @@ client.on('messageCreate', async message => {
     }
 });
 
+// تشغيل سيرفر وهمي لرضا منصة Render
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 client.login(process.env.TOKEN);
